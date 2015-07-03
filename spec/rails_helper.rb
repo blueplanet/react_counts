1cf2ae65922b85c3458e74e86843199778e48606
blueplanet <erguolinge@gmail.com>
2015-07-03 19:52:04 +0900

    rspec設定追加

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,7 +5,21 @@ require 'rspec/rails'
 
 ActiveRecord::Migration.maintain_test_schema!
 
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
 RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
+  config.include FactoryGirl::Syntax::Methods
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with :truncation
+    FactoryGirl.reload
+  end
+
+  config.before(:each, js: true) { DatabaseCleaner.strategy = :truncation }
+  config.after(:each, js: true) { DatabaseCleaner.strategy = :transaction }
+  config.before(:each) { DatabaseCleaner.start }
+  config.after(:each) { DatabaseCleaner.clean }
 end
